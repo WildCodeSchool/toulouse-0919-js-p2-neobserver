8bafb79e61604988aee8dfefff06139e40966f2c
Morgane7260 <morgane7260@gmail.com>
2019-10-29 12:02:51 +0100

    Modification de l'index, à modifier plus tard. Modification de App, à modifier plus tard

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
+/* eslint-disable no-undef */
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer';
-import {Switch, Route} from "react-router-dom";
 
-class App extends React.Component{
-
-  render(){
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
     return (
-    <div>
-      <Switch>
-        <Route exact-path="/" component={Home}/>
-        <Route exact-path="/" component={}/>
-        <Route exact-path="/" component={}/>
-        <Route exact-path="/" component={}/>
-        <Route exact-path="/" component={}/>
-      </Switch>
-      <Footer />
-    </div>
-    )
-}
+      <div>
+        <Switch>
+          <Route exact-path="/" component={Home} />
+          <Route exact-path="/" component={newsPage} />
+          <Route exact-path="/" component={FilterResults} />
+          <Route exact-path="/" component={Glossary} />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
+  // eslint-disable-next-line no-undef
   document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
