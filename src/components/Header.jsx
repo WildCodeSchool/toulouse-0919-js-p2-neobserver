2f42aafe00ebf7c9cd288046d144ede6b643325e
Julien B <gercevalcode@gmail.com>
2019-10-29 16:03:07 +0100

    added parallax

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Parallax } from 'react-parallax';
 import './Header.css';
-
-const image1 =
-  'https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D';
+import wallpaper from '../assets/wallpaper.jpg';
 
 function Header() {
   return (
-    <Parallax bgImage={image1} strength={800}>
+    <Parallax bgImage={wallpaper} strength={500}>
       <div className="Header">
         <h1 className="NEOBSERVER">NEOBSERVER</h1>
       </div>
