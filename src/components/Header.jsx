dfb99cecca8943e7500ad9a113351fea03beac9e
Julien B <gercevalcode@gmail.com>
2019-10-29 17:10:15 +0100

    trie changing font family

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Parallax } from 'react-parallax';
 import './Header.css';
 import wallpaper from '../assets/wallpaper.jpg';
+import 'typeface-roboto';
 
 function Header() {
   return (
-    <Parallax bgImage={wallpaper} strength={500}>
+    <Parallax bgImage={wallpaper} strength={400}>
       <div className="Header">
         <h1 className="NEOBSERVER">NEOBSERVER</h1>
       </div>
