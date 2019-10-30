2f537fe9b7dc6b5db7afe2a017d1d186c4a6b7f8
Morgane7260 <morgane7260@gmail.com>
2019-10-30 09:23:43 +0100

    Ajout du header, modification du css de displayNews nécessaire

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CardNeo from './CardNeo';
 import News from './News';
+import Header from './Header';
 
 function Home() {
   return (
     <div>
+      <Header />
       <CardNeo />
       <News />
     </div>
