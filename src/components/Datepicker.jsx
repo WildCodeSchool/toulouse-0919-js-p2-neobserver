06897eeb371e9da497f1bb0592551310132920aa
Julien B <gercevalcode@gmail.com>
2019-11-05 12:21:26 +0100

    created getNeos method with axios

--- src/components/Datepicker.jsx
+++ src/components/Datepicker.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 function getEndDate(startDate) {
   const copy = new Date(startDate);
@@ -14,10 +15,31 @@ class Datepicker extends Component {
       arrayResults: null
     };
     this.handleChange = this.handleChange.bind(this);
+    this.getNeos = this.getNeos.bind(this);
+  }
+
+  getNeos() {
+    if (this.state.arrayDate !== null) {
+      axios
+        .get(
+          `https://api.nasa.gov/neo/rest/v1/feed?
+        start_date=${this.state.arrayDate[0]}&
+        end_date=${this.state.arrayDate[1]}&
+        api_key=Jr2an55MZH7pzYVTcL1DvZazu0oEgJils4o4KdjI`
+        )
+        .then(response => response.data)
+        .then(data => {
+          this.setState({
+            arrayResults: [data.near_earth_objects[`${this.arrayDate[0]}`]]
+          });
+        });
+    }
   }
 
   handleChange(event) {
     this.setState({ arrayDate: [event.target.value, getEndDate(event.target.value)] });
+    this.getNeos();
+    console.log(this.arrayResults);
   }
 
   render() {
