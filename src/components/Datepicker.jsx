1ec9f407e28bbdf289ed2fd8c1e35024af0b625b
Julien B <gercevalcode@gmail.com>
2019-11-06 09:27:10 +0100

    affichage d'un neo en fonction d'une date user

--- src/components/Datepicker.jsx
+++ src/components/Datepicker.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import DisplayCardNeoOTD from './DisplayCardNeoOTD';
 
 function getEndDate(startDate) {
   const copy = new Date(startDate);
@@ -19,27 +20,23 @@ class Datepicker extends Component {
   }
 
   getNeos() {
-    if (this.state.arrayDate !== null) {
-      axios
-        .get(
-          `https://api.nasa.gov/neo/rest/v1/feed?
-        start_date=${this.state.arrayDate[0]}&
-        end_date=${this.state.arrayDate[1]}&
-        api_key=Jr2an55MZH7pzYVTcL1DvZazu0oEgJils4o4KdjI`
-        )
-        .then(response => response.data)
-        .then(data => {
-          this.setState({
-            arrayResults: [data.near_earth_objects[`${this.arrayDate[0]}`]]
-          });
+    axios
+      .get(
+        `https://api.nasa.gov/neo/rest/v1/feed?start_date=${this.state.arrayDate[0]}&end_date=${
+          this.state.arrayDate[1]
+        }&api_key=Jr2an55MZH7pzYVTcL1DvZazu0oEgJils4o4KdjI`
+      )
+      .then(response => response.data)
+      .then(data => {
+        this.setState({
+          arrayResults: data.near_earth_objects[`${this.state.arrayDate[0]}`]
         });
-    }
+      });
   }
 
   handleChange(event) {
     this.setState({ arrayDate: [event.target.value, getEndDate(event.target.value)] });
     this.getNeos();
-    console.log(this.arrayResults);
   }
 
   render() {
@@ -48,6 +45,7 @@ class Datepicker extends Component {
       <div>
         <input type="date" value={arrayDate ? arrayDate[0] : ''} onChange={this.handleChange} />
         <input type="date" value={arrayDate ? arrayDate[1] : ''} disabled />
+        {this.state.arrayResults && <DisplayCardNeoOTD neo={this.state.arrayResults[0]} />}
       </div>
     );
   }
