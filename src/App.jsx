ad783ab3f173b77fdb2c8c6336a0e0ea0a7cea8c
AnnaMarandola <annamarandola888@gmail.com>
2019-11-09 20:28:37 +0100

    creation du composant et routage

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,18 @@ import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Home from './components/Home';
 import AllNews from './components/AllNews';
-import ParentFilters from './components/ParentFilters';
-
+import Glossary from './components/Glossary';
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
       <div>
         <Nav />
-        <ParentFilters />
         <Switch>
           <Route exact path="/" component={Home} />
           {/* <Route path="/" component={Neos} /> */}
           <Route path="/AllNews" component={AllNews} />
-          {/* <Route path="/" component={Glossary} /> */}
+          <Route path="/Glossary" component={Glossary} />
         </Switch>
         <Footer />
       </div>
---
+++ src/components/Glossary.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Glossary = () => {
+  return (
+    <div>
+      <h1>Glossary</h1>
+      <div>
+        <h3> </h3>
+        <img> </img>
+        <p> </p>
+      </div>
+    </div>
+  );
+};
+
+export default Glossary;
