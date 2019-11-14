217c846d0f26364bfc5f6c2de0da03d900d89c05
coralie Rubin <rubin.coralie@gmail.com>
2019-11-14 14:15:31 +0100

    Css btn en cours

--- src/components/Datepicker.jsx
+++ src/components/Datepicker.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { getFlattenArrayLength } from './utils/utils';
+import './AllFilter.css';
 
 const Datepicker = ({ handlerCreateArrayDate, arrayDate, getNeos, arrayResults }) => {
   return (
-    <div>
+    <div className="CalendarGlobal">
       <input
+        className="CalendarStart"
         type="date"
         value={arrayDate ? arrayDate[0] : ''}
         onChange={event => handlerCreateArrayDate(event)}
       />
-      <input type="date" value={arrayDate ? arrayDate[7] : ''} disabled />
+      <input className="CalendarEnd" type="date" value={arrayDate ? arrayDate[7] : ''} disabled />
       <button type="button" onClick={getNeos}>
         Search by week
       </button>
-      <p>{arrayResults && `We found ${getFlattenArrayLength(arrayResults)} NEOs`}</p>
+      <p className="CalendarBtn">
+        {arrayResults && `We found ${getFlattenArrayLength(arrayResults)} NEOs`}
+      </p>
     </div>
   );
 };
