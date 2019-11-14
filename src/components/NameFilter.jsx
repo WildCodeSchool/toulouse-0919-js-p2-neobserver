bff2e0e29b1bd3b551f2d6a25aca81c4521aa542
Morgane7260 <morgane7260@gmail.com>
2019-11-14 17:11:45 +0100

    Modification bouton

--- src/components/NameFilter.jsx
+++ src/components/NameFilter.jsx
@@ -3,18 +3,15 @@ import DisplayCardNeo from './DisplayCardNeo';
 
 const NameFilter = ({ handleNeoByName, findNeoByName, searchedInputName, foundNeo }) => {
   return (
-    <div className="Filter-Name">
+    <div>
       <label htmlFor="name"> Name</label>
       <input
-        className="Input-Name"
         onChange={event => handleNeoByName(event)}
         id="searchName"
         type="text"
         value={searchedInputName}
       />
-      <button className="Btn-Name" onClick={() => findNeoByName(searchedInputName)}>
-        Search
-      </button>
+      <button onClick={() => findNeoByName(searchedInputName)}>SEARCH</button>
       {foundNeo && <DisplayCardNeo infoNeo={foundNeo} />}
     </div>
   );
